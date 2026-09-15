06155bfe9080392d1252c653a88118e7e8617363
Felipe Kume <80411349+felipekume@users.noreply.github.com>
2026-09-15 13:07:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
+function abrirJanela() {
+  const win = new BrowserWindow({ width: 1300, height: 900, title: 'Resumo Fiscal', autoHideMenuBar: true });
+  win.loadFile(path.join(__dirname, 'index.html'));
+}
+
+app.whenReady().then(abrirJanela);
+app.on('window-all-closed', () => app.quit());
